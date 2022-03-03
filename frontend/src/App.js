ab47bfa0af736e30a239acc28f3c7ac772573707
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2022-03-03 02:06:02 -0700

    basic setup of routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+ import React from 'react';
+ import {
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Pokedex from './Pokedex';
+import Pokemon from './Pokemon';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+ <Routes>
+   {/* <Route exact path='/' element={(props)=><Pokedex{...props}/>} />
+   <Route  path='/:pokeid' element={(props)=><Pokemon{...props}/>}/> */}
+   <Route  path='/' element={<Pokedex/>}/>
+   <Route  path='/:pokeid' element={<Pokemon/>}/>
+ </Routes> 
+   
   );
 }
 
---
+++ frontend/src/Pokedex.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const  Pokedex=()=>{
+    return(
+        <div>
+            This is Pokedex 
+        </div>
+    )
+}
+
+export default Pokedex
