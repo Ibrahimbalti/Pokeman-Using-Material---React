b245948e45576cddee89bfce4c8ad5cc6244822a
Muhammad Ibrahim <ibrahimraju347@gmail.com>
2022-03-03 03:30:00 -0700

    creating the layout(grid + cards)

--- frontend/src/Pokedex.js
+++ frontend/src/Pokedex.js
@@ -1,9 +1,41 @@
 import React from 'react'
-const  Pokedex=()=>{
-    return(
-        <div>
-            This is Pokedex 
-        </div>
+import { AppBar, Toolbar, Grid, Card,CardContent } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyle = makeStyles({
+    pokedexContainer: {
+        paddingTop: '20px',
+        paddingLeft: '20px',
+        paddingRight: '20px'
+    }
+})
+
+const getpokemancard = () => {
+return(
+    <Grid item sm={4}>
+        <Card>
+            <CardContent>Hello</CardContent>
+        </Card>
+    </Grid>
+)
+}
+const Pokedex = () => {
+
+    const classes = useStyle()
+    return (
+        <>
+            <AppBar position='static '>
+                <Toolbar></Toolbar>
+            </AppBar>
+            <Grid container spacing={4} className={classes.pokedexContainer}>
+               {getpokemancard()} 
+               {getpokemancard()} 
+               {getpokemancard()} 
+               {getpokemancard()} 
+
+            </Grid>
+
+        </>
     )
 }
 
--- frontend/src/Pokemon.js
+++ frontend/src/Pokemon.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 const Pokemon=()=>{
+    const {pokeid} =useParams()
     return(
         <div>
-            This is Pokemon page
+           <h1>This is Pokemon page {pokeid}</h1> 
         </div>
     )
 }
